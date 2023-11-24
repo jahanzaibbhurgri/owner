3da47538cee4a973c5a2b8657100295abafa2555
jahanzaibbhurgri <jahanzaibbhurgri1@gmail.com>
2023-11-24 18:22:49 +0500

    sasdq

--- src/server.ts
+++ src/server.ts
@@ -10,7 +10,7 @@ import swaggerExport from "./config";
 
 // Create an instance of Express
  export const app: Express = express();
-const port: number = 5432 ||3000;
+const port: number = 3000;
 
 app.use(bodyParser.json());
 app.use('/api-docs', swaggerExport.swaggerServe,swaggerExport.swaggerSetup);
